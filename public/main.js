f9a219ff49bd806d6aa7ddcbb16c5edc472bdca9
Anuraj Shrestha <anurajshrestha75@gmail.com>
2024-02-28 06:54:12 -0500

    made functionaly on search and css on search bar

--- public/main.js
+++ public/main.js
@@ -54,3 +54,52 @@ contact.addEventListener('submit', async(e) =>{
         console.log(error)
     }
 })
+
+
+function myFunction() {
+
+    let input, filter, courses, courseInfo, i, txtValue;
+
+    input = document.getElementById("myInput");
+
+    filter = input.value.toUpperCase();
+
+    courses = document.getElementById("myCourses");
+
+    courseInfo = courses.getElementsByTagName("article");
+
+    for (i = 0; i < courseInfo.length; i++) {
+
+        txtValue = courseInfo[i].getElementsByTagName("h4")[0].textContent || courseInfo[i].getElementsByTagName("h4")[0].innerText;
+
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+
+            courseInfo[i].style.display = "";
+
+        } else {
+
+            courseInfo[i].style.display = "none";
+
+        }
+
+    }
+
+}
+//     var input, filter, div, i, info, txtValue
+//     input = document.getElementById('myInput')
+//     filter = input.value.toUpperCase()
+//     div = document.getElementById("value")
+//      course = document.getElementsByTagName('coursesinfo')
+
+//     for( i=0; i<div.length; i++ ){
+//         info = div[i].course.getElementsByTagName("h4")[0];
+//         txtValue = info.textContent || arguments.innerText;
+
+//         if(txtValue.toUpperCase().indexOf(filter) > -1){
+//             course[i].style.display = ""
+//         }
+//         else {
+//             course[i].style.display = "none"
+//         }
+//     }
+// }
